5b36c717283dfffb08b8383741fe377e401a5880
filipslunsky <fslunsky@gmail.com>
2024-11-30 18:53:21 +0100

    finished styling mobile navbar

--- client/src/features/Navbar.jsx
+++ client/src/features/Navbar.jsx
@@ -1,22 +1,35 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from "./Logo";
 import cartIcon from '../assets/img/icon-cart.svg';
 import avatarImage from '../assets/img/image-avatar.png';
+import mobileMenuIcon from '../assets/img/icon-menu.svg';
 import './navbar.css';
 
 
 const Navbar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    const closeMobileMenu = () => {
+        setIsMobileMenuOpen(false);
+    };
+
     return (
     <>
         <div className='navContainer'>
             <div className='leftContainer'>
+            <img className='menuIcon' src={mobileMenuIcon} alt="menu icon" onClick={toggleMobileMenu} />
                 <Logo />
-                <div className='navItemsContainer'>
-                    <Link className='navItem' to={'/collections'}>Collections</Link>
-                    <Link className='navItem' to={'/men'}>Men</Link>
-                    <Link className='navItem' to={'/women'}>Women</Link>
-                    <Link className='navItem' to={'/about'}>About</Link>
-                    <Link className='navItem' to={'/contact'}>Contact</Link>
+                <div className={`navItemsContainer ${isMobileMenuOpen ? 'mobileMenu' : ''}`}>
+                    <Link onClick={closeMobileMenu} className='navItem' to={'/collections'}>Collections</Link>
+                    <Link onClick={closeMobileMenu} className='navItem' to={'/men'}>Men</Link>
+                    <Link onClick={closeMobileMenu} className='navItem' to={'/women'}>Women</Link>
+                    <Link onClick={closeMobileMenu} className='navItem' to={'/about'}>About</Link>
+                    <Link onClick={closeMobileMenu} className='navItem' to={'/contact'}>Contact</Link>
                 </div>
             </div>
             <div className='rightContainer'>

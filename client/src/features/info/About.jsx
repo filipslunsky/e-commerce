82ee1b82d3c9405e120723f85bfd77a5aef8d72c
filipslunsky <fslunsky@gmail.com>
2024-12-02 11:10:39 +0100

    added contact and info content + styling

--- client/src/features/info/About.jsx
+++ client/src/features/info/About.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+import './infoPage.css';
+
 const About = () => {
     return (
         <>
-            <h2>About</h2>
+            <div className="infoContainer">
+                <h2 className="infoHeader">About</h2>
+                <p className="infoParagraph">This is a React e-shop with responsive styling done purely in CSS, no external libraries. The state management on this website is handled with Redux Toolkit. All data is fetched from an Express Node.js server.</p>
+                <p className="infoParagraph">If you wish to see more of my work, feel free to <br /> <Link className="internalAnchor" to="/contact">contact me.</Link></p>
+            </div>
         </>
     );
 }
--- client/src/features/info/Contact.jsx
+++ client/src/features/info/Contact.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+import './infoPage.css';
+
 const Contact = () => {
     return (
         <>
-            <h2>Contact</h2>
+            <div className="infoContainer">
+                <h2 className="infoHeader">Contact Info</h2>
+                <a target="_blank" className="externalAnchor" href="https://www.filipslunsky.com/">My Portfolio</a>
+                <a target="_blank" className="externalAnchor" href="https://github.com/filipslunsky">My GitHub</a>
+                <a target="_blank" className="externalAnchor" href="https://www.linkedin.com/in/filipslunsky/">Connect with me on my LinkedIn</a>
+                <hr />
+                <a target="_blank" className="externalAnchor" href="https://github.com/filipslunsky/e-commerce">GitHub Repository for this E-Shop</a>
+                <Link className="internalAnchor" to="/about">About this E-shop</Link>
+            </div>
         </>
     );
 }
